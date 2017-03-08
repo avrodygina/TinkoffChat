9eca46aef9b8f5bd201df458421347c3c297842e
avrodygina <nastyachka98@mail.ru>
2017-03-08 17:51:53 +0300

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tinkoff_Chat
+//
+//  Created by rav on 08.03.17.
+//  Copyright © 2017 rav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
